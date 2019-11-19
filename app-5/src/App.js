dcff92d80aca775045b4d73f7cf77501a776d723
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2019-11-19 16:01:13 -0700

    image added to app

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Image from './Image'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      URL: 'https://www.touristisrael.com/wp-content/uploads/Petra-_-Wadi-Rum-2-Day-Tour-from-Eilat-3.jpg'
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image link={this.state.URL}/>
       </div>
     );
   }
