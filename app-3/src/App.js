010cdc731483a1d731460eae67279e5bf69a395e
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2019-12-17 07:36:04 -0700

    app-3 working

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,32 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      filterString: '',
+      greetings: ['hello', 'salam', 'bon jour', 'hola', 'aloha', 'namaste']
+    }
+  }
+
+  changeHandler(filter) {
+    this.setState({filterString: filter});
+  };
+
   render() {
+    const {greetings} = this.state;
+    const mappedGreetings = greetings.filter(greeting => {
+      return greeting.includes(this.state.filterString)
+    }).map(greeting => {
+    return <h3 key={greeting.index} >{greeting}</h3>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <input onChange={e => this.changeHandler(e.target.value)} type='text'  />
+        <div>
+          {mappedGreetings}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
