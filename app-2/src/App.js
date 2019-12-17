47ce324bada992e43c8069531b33b4cabf8c0573
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2019-12-17 07:24:12 -0700

    app-2 displaying array on state

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,22 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      greetings: ['hello', 'salam', 'bon jour', 'hola', 'aloha', 'namaste']
+    }
+  }
+
   render() {
+    const {greetings} = this.state;
+    const mappedGreetings = greetings.map(greeting => {
+    return <h3 key={greeting.index} >{greeting}</h3>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {mappedGreetings}
       </div>
     );
   }
